debb5b675c264f297ebaac3fe5801d1b4aed0fc9
Divakar Singh <48847293+divakar166@users.noreply.github.com>
2021-12-11 15:55:37 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -19,9 +19,12 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from main import Ui_MainWindow
 from datetime import datetime
+import urllib.request
+import urllib.parse
+import re
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[1].id)
 now = datetime.now()
 
 class MainThread(QThread):
@@ -71,7 +74,7 @@ class MainThread(QThread):
     def querySolver(self):
         while True:
             query = self.takeCommand().lower()
-            if 'quit' in query or 'close' in query or 'exit' in query or 'quit Zero' in query or 'close Zero' in query or 'exit Zero' in query or 'bye' in query or 'bye Zero' in query:
+            if 'quit' in query or 'exit' in query or 'quit Zero' in query or 'exit Zero' in query or 'bye' in query or 'bye Zero' in query:
                 self.speak("Thank you Sir for your time .  !!!! Have a nice day. ...")
                 break
             elif 'hello' in query or 'hello Zero' in query or 'hi Zero' in query or 'hi' in query or 'hey Zero' in query or 'hey' in query:
@@ -96,30 +99,60 @@ class MainThread(QThread):
             elif 'send message' in query:
                 pywhatkit.sendwhatmsg("+916395047573", "Hello from GeeksforGeeks", now.hour, now.minute+1)
                 print("Successfully Sent!")
-            elif 'open notepad' in query or 'notepad' in query:
+            elif 'open notepad' in query:
                 self.speak('Opening Notepad')
                 os.system("start notepad")
-            elif 'open wordpad' in query or 'wordpad' in query:
+            elif 'close notepad' in query:
+                self.speak("Closing Notepad")
+                os.system("taskkill /f /im notepad.exe")
+            elif 'open wordpad' in query:
                 self.speak("Opening Wordpad")
                 os.system("start wordpad")
-            elif 'open excel' in query or 'open msexcel' in query:
+            elif 'close wordpad' in query:
+                self.speak("Closing Wordpad")
+                os.system("taskkill /f /im wordpad.exe")
+            elif 'open excel' in query:
                 self.speak("Opening MS Excel")
                 os.system("start excel")
-            elif 'open access' in query or 'open msaccess' in query:
+            elif 'close excel' in query:
+                self.speak("Closing MS Excel")
+                os.system("taskkill /f /im excel.exe")
+            elif 'open powerpoint' in query:
+                self.speak("Opening PowerPoint")
+                os.system("start powerpnt")
+            elif 'close powerpoint' in query:
+                self.speak("Closing powerpoint")
+                os.system("taskkill /f /im powerpnt.exe")
+            elif 'open access' in query:
                 self.speak("Opening MS Access")
                 os.system("start msaccess")
+            elif 'close access' in query:
+                self.speak("Closing MS Access")
+                os.system("taskkill /f /im msaccess.exe")
             elif 'open browser' in query:
                 self.speak("Opening Browser")
                 os.system("start chrome")
+            elif 'close browser' in query:
+                self.speak("Closing Browser")
+                os.system("taskkill /f /im chrome.exe")
             elif 'open chrome' in query:
                 self.speak("Opening Chrome")
                 os.system("start chrome")
+            elif 'close browser' in query:
+                self.speak("Closing Browser")
+                os.system("taskkill /f /im chrome.exe")
             elif 'open calculator' in query:
                 self.speak("Opening Calculator")
                 os.system("start calc")
+            elif 'close calculator' in query:
+                self.speak("Closing Calculator")
+                os.system("taskkill /f /im calculator.exe")
             elif 'open firefox' in query:
                 self.speak("Opening Firefox")
                 os.system("start firefox")
+            elif 'close firefox' in query:
+                self.speak("Closing firefox")
+                os.system("taskkill /f /im firefox.exe")
             elif 'open control panel' in query or 'control panel' in query:
                 self.speak("Opening Control Panel")
                 os.system("start control panel")
@@ -127,6 +160,9 @@ class MainThread(QThread):
                 self.speak("Opening Camera")
                 print("Opening Camera")
                 os.system("start microsoft.windows.camera:")
+            elif 'close camera' in query:
+                self.speak("Closing Camera")
+                os.system("taskkill /f /im windowscamera.exe")
             elif 'time' in query or 'what is time' in query:
                 strTime = datetime.datetime.now().strftime("%H:%M:%S")
                 self.speak(f"Sir, the time is {strTime}")
@@ -134,36 +170,61 @@ class MainThread(QThread):
                 self.speak("salman khan is a heavy driver")
             elif 'what are you doing Zero' in query:
                 self.speak("i am searching for your favorite songs ")
-            elif 'thank you Zero ' in query or 'thank you' in query:
+            elif 'thank you Zero' in query or 'thank you' in query:
                 self.speak("Welcome Sir !!! ... .")
-            elif 'open youtube' in query:
-                webbrowser.open("www.youtube.com")
+            elif 'play' in query or 'youtube' in query or 'in youtube' in query or 'on youtube' in query:
+                dec = {"on youtube":"","in youtube":"","youtube":""," ":"+","play":""}
+                for key,value in dec.items():
+                    query = query.replace(key,value)
+                query_string = urllib.parse.urlencode({"search_query": f"{query}"})
+                html_content = urllib.request.urlopen("https://www.youtube.com.hk/results?" + query_string)
+                search_results = re.findall(r'url\"\:\"\/watch\?v\=(.*?(?=\"))', html_content.read().decode())
+                if search_results:
+                    webbrowser.open_new("http://www.youtube.com/watch?v={}".format(search_results[0]))
             elif 'open facebook' in query:
                 webbrowser.open("www.facebook.com")
             elif 'how are you' in query:
                 speak("i'm fine, how's your day going ?")
             elif 'good' in query:
-                speak("sound good. | let me know of you need anything. ")
+                speak("sound good. let me know if you need anything. ")
             elif 'ok' in query:
                 speak("okk  !!!  Sir  ")
             elif 'who are you' in query:
-                speak("I am a virtual assistant created to do ")
+                speak("I am a virtual assistant created to do certain tasks.")
             elif 'who is salman khan' in query:
                 speak("salman khan is a heavy driver")
-            elif 'what are you doing friday ' in query:
-                speak("i am searching for your favorite songs ")
-            elif 'open google' in query:
-                webbrowser.open("www.google.com")
-                self.speak("sir,what should i search on google")
-                cm = self.takeCommand().lower()
-                webbrowser.open(f"{cm}")
-
+            elif 'what are you doing zero ' in query:
+                speak("i am learning something new!")
+            elif 'open spotify' in query or 'spotify' in query:
+                self.speak("Opening Spotify!")
+                webbrowser.open("www.open.spotify.com")
+            elif 'on google' in query or 'in google' in query or 'google' in query:
+                g_dec = {"in google":"","on google":"","google":""," ":"+"}
+                for key,value in g_dec.items():
+                    query = query.replace(key,value)
+                webbrowser.open("www.google.com/search?q="+f"{query}")
+            elif 'who is' in query:
+                self.speak('Searching Wikipedia.......')
+                query = query.replace("who is","")
+                results = wikipedia.summary(query, sentences=2)
+                self.speak("According to Wikipedia ")
+                print(results)
+                self.speak(results)
+            elif 'what is' in query:
+                self.speak('Searching Wikipedia.......')
+                query = query.replace("what is","")
+                results = wikipedia.summary(query, sentences=2)
+                self.speak("According to Wikipedia ")
+                print(results)
+                self.speak(results)
             elif 'open stackoverflow' in query:
-                webbrowser.open("stackoverflow.com")
+                webbrowser.open_new("stackoverflow.com")
             elif 'open' in query:
                 self.speak("What should I open, Sir!")
+            elif '' in query:
+                self.speak('Anything for me, Sir!')
             else:
-                self.speak("Anything for me Sir!")
+                self.speak("Say that again please!")
 
 startExecution = MainThread()
 
@@ -195,17 +256,14 @@ class Main(QMainWindow):
         self.ui.pushButton.clicked.connect(self.stopMainTask)
 
     def stopMainTask(self):
-        self.ui.movie = QtGui.QMovie("C:\\Users\\vashishtha\\Downloads\\New\\Hologram 3.gif")
+        self.ui.movie = QtGui.QMovie("C:\\Users\\vashishtha\\Downloads\\New\\d13449fb76b34cb71584f5bfb7c6dee9.gif")
+        self.ui.label_2.setMovie(self.ui.movie)
+        self.ui.movie.stop()
+        self.ui.movie = QtGui.QMovie("C:\\Users\\vashishtha\\Downloads\\New\\59d39ea8cfb59-unscreen.gif")
         self.ui.label_4.setMovie(self.ui.movie)
         self.ui.movie.stop()
-        self.ui.movie = QtGui.QMovie("C:\\Users\\vashishtha\\Downloads\\New\\75lD.gif")
-        self.ui.label_6.setMovie(self.ui.movie)
-        self.ui.movie.stop()
-        self.ui.movie = QtGui.QMovie("C:\\Users\\vashishtha\\Downloads\\New\\2number.gif")
-        self.ui.label_7.setMovie(self.ui.movie)
-        self.ui.movie.stop()
-        self.ui.movie = QtGui.QMovie("C:\\Users\\vashishtha\\Downloads\\New\\sw.gif")
-        self.ui.label_8.setMovie(self.ui.movie)
+        self.ui.movie = QtGui.QMovie("C:\\Users\\vashishtha\\Downloads\\New\\radiohalo-800.gif")
+        self.ui.label_5.setMovie(self.ui.movie)
         self.ui.movie.stop()
         startExecution.stop()
 
